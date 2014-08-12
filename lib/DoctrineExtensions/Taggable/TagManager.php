c432e08e9ab561b1e3f717710d4a2fed77ccd825
Juan Manuel <juanmf@juanmf-V570.(none)>
2014-08-12 14:07:15 -0300

    added optional parameter to prevent flushing new missing tags to database. Refactor loadOrCreateTags() a bit

--- lib/DoctrineExtensions/Taggable/TagManager.php
+++ lib/DoctrineExtensions/Taggable/TagManager.php
@@ -100,19 +100,37 @@ class TagManager
     /**
      * Loads or creates multiples tags from a list of tag names
      *
-     * @param array  $names   Array of tag names
+     * @param array  $names          Array of tag names
+     * @param bool   $persistMissing Determines if tags not present in DB should be
+     * persisted. In filter Forms scenario you dont want to create tags that are 
+     * used for filtering purpse only.
+     * 
      * @return Tag[]
      */
-    public function loadOrCreateTags(array $names)
+    public function loadOrCreateTags(array $names, $persistMissing = true)
     {
         if (empty($names)) {
             return array();
         }
-
         $names = array_unique($names);
+        
+        $tags = $this->fetchTags($names);
+        $missingNames = $this->computeMissingNames($names, $tags);
+        $tags = array_merge($tags, $this->createTags($missingNames, $persistMissing));
+        
+        return $tags;
+    }
 
+    /**
+     * Makes a DB query to get the list of tags by Tag names.
+     * 
+     * @param array $names Array of tag names
+     * 
+     * @return Tag[] The tags found in DB.
+     */
+    protected function fetchTags(array $names)
+    {
         $builder = $this->em->createQueryBuilder();
-
         $tags = $builder
             ->select('t')
             ->from($this->tagClass, 't')
@@ -122,27 +140,59 @@ class TagManager
             ->getQuery()
             ->getResult()
         ;
-
+        return $tags;
+    }
+    
+    /**
+     * Gets the difference between desired tags and found tags.
+     * 
+     * @param array $names All desired Tag names.
+     * @param array $tags  Tags found on DB.
+     * 
+     * @return array The missing names, that were not found in DB.
+     */
+    protected function computeMissingNames(array $names, array $tags)
+    {
         $loadedNames = array();
         foreach ($tags as $tag) {
             $loadedNames[] = $tag->getName();
         }
 
-        $missingNames = array_udiff($names, $loadedNames, 'strcasecmp');
-        if (sizeof($missingNames)) {
-            foreach ($missingNames as $name) {
-                $tag = $this->createTag($name);
+        return array_udiff($names, $loadedNames, 'strcasecmp');
+    }
+    
+    /**
+     * Create Tag objects as per $names list, optionally persistnign them to DB.
+     * 
+     * @param array $names   Tag names to set to new Tag objects.
+     * @param bool  $persist Determines if created Tags must be persisted.
+     * 
+     * @return type
+     */
+    protected function createTags(array $names, $persist)
+    {
+        if (! sizeof($names)) {
+            return array();
+        }
+        $tags = array();
+        foreach ($names as $name) {
+            $tag = $this->createTag($name);
+            if ($persist) {
                 $this->em->persist($tag);
-
-                $tags[] = $tag;
+                $this->em->flush($tag);
             }
 
-            $this->em->flush();
+            $tags[] = $tag;
         }
-
+        
+        if ($persist) {
+            // TODO: remove this if, or bring flush() back here, as I moved it up
+            // as flush()  with no params also flushes other managed dirty objects,
+            // that might not be intended to be updated now.
+        }
         return $tags;
     }
-
+    
     /**
      * Saves tags for the given taggable resource
      *
